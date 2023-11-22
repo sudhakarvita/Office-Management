9ccdc62029f880d7d9c601f6787b6b4620dc7eab
sudhakarvita <sudhakar.vita99@gmail.com>
2023-11-22 14:57:28 +0530

    created employees api

--- Routes/employeerouter.js
+++ Routes/employeerouter.js
@@ -28,4 +28,50 @@ router.post( '/employee/login',cors(corsOptions), async(req,res) =>{
     }
   });
 
+  //get all admins
+
+router.get('/get/employees', async (req, res) => {
+    try {
+      const allEmployees = await employee.find(); 
+      res.status(201).json(allEmployees);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  });
+
+  // get employee with id
+
+router.get('/get/employee/:id', async (req, res) => {
+    try {
+      const Employee = await employee.findById(req.params.id);
+      res.status(200).json(Employee);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  });
+
+  //employee update with id
+
+router.put( '/employee/update/:id', async (req,res) =>{
+    try{
+      const Employee = await employee.findByIdAndUpdate( req.params.id, {$set:req.body}, {new:true});
+      res.status(201).json(Employee);
+    }catch(err){
+      res.status(500).json(err)
+    }
+  });
+
+  //delete employee with id
+
+router.delete( '/employee/delete/:id', async (req,res) =>{
+    try{
+      const Employee = await employee.findByIdAndDelete(req.params.id);
+      res.status(201).json(Employee);
+    }catch(err){
+      res.status(500).json(err);
+    } 
+  });
+
 module.exports = router
